c073ecbf24bbbaee3e4372596963877d5b5fd122
Mkhs <jonbecher@microagenetworks.com>
2020-02-28 04:59:17 -0500

    Created function and main logic of application

---
+++ 04_Personal_Journal_App/program.py
@@ -0,0 +1,17 @@
+
+def print_header():
+    print('------------------------------')
+    print('        JOURNAL APP')
+    print('------------------------------')
+    print()
+
+
+def run_event_loop():
+    pass
+
+
+def main():
+    print_header()
+    run_event_loop()
+
+
