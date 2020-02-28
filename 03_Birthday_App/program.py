60d1865982538b96a411e730b18c23844fb347f4
Mkhs <jonbecher@microagenetworks.com>
2020-02-27 22:58:45 -0500

    Completed logic for computing days between the dates and printing birthday information

--- 03_Birthday_App/program.py
+++ 03_Birthday_App/program.py
@@ -18,18 +18,27 @@ def get_birthday_from_user():
     return bday
 
 
-def compute_days_between_dates():
-    pass
+def compute_days_between_dates(original_date, target_date):
+    this_year = datetime.date(target_date.year, original_date.month, original_date.day)
+    
+    dt = this_year - target_date
+    return dt.days
 
 
-def print_birthday_information():
-    pass
+def print_birthday_information(days):
+    if days < 0:
+        print('you had your birthday {} days ago this year.'.format(-days))
+    elif days > 0:
+        print('Your birthday is in {} days!'.format(days))
+    else:
+        print('Happy birthday!')
+
 
 def main():
     print_header()
     bday = get_birthday_from_user()
-    now = None
-    number_of_days = compute_days_between_dates(bday, now)
+    today = datetime.date.today()
+    number_of_days = compute_days_between_dates(bday, today)
     print_birthday_information(number_of_days)
 
 
