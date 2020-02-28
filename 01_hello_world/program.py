5dcd629186779916d472115f649fbc957c05926f
Mkhs <jonbecher@microagenetworks.com>
2020-02-27 21:03:16 -0500

    Added variable storage for input; Added Greeting and .format, then printed it to the console

--- 01_hello_world/program.py
+++ 01_hello_world/program.py
@@ -3,4 +3,7 @@ print('          HELLO APP')
 print('------------------------------')
 print()
 
-input('What is your name? ')
+user_text = input('What is your name? ')
+greeting = 'Nice to meet you {}'.format(user_text)
+
+print(greeting)
