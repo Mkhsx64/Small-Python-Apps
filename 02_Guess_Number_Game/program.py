ac44d02c03c974484edb6fb6a54e299a1bfe0c9e
Mkhs <jonbecher@microagenetworks.com>
2020-02-27 22:04:10 -0500

    Created While Loop for Multiple Guessing

--- 02_Guess_Number_Game/program.py
+++ 02_Guess_Number_Game/program.py
@@ -6,14 +6,17 @@ print('-----------------------------')
 print()
 
 the_number = random.randint(0, 100)
+guess = -1
 
-guess_text = input('Guess a number between 0 and 100: ')
+while guess != the_number:
+    guess_text = input('Guess a number between 0 and 100: ')
+    guess = int(guess_text)
+    
+    if guess < the_number:
+        print('too low')
+    elif guess > the_number:
+        print('too high')
+    else:
+        print('you win!')
 
-guess = int(guess_text)
-
-if guess < the_number:
-    print('too low')
-elif guess > the_number:
-    print('too high')
-else:
-    print('you win!')
+print('done')
