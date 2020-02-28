f73e14e814e1ea70dfe3f38cac656ed5626c9222
Mkhs <jonbecher@microagenetworks.com>
2020-02-27 21:06:36 -0500

    Fixed Grammer

--- 01_hello_world/program.py
+++ 01_hello_world/program.py
@@ -4,6 +4,6 @@ print('------------------------------')
 print()
 
 user_text = input('What is your name? ')
-greeting = 'Nice to meet you {}'.format(user_text)
+greeting = 'Nice to meet you {}!'.format(user_text)
 
 print(greeting)
