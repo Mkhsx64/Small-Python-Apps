6fa58f4270839f37dbdf4d433d6087cdfb67cbf1
Mkhs <jonbecher@microagenetworks.com>
2020-02-27 22:01:26 -0500

    Created if statement logic

--- 02_Guess_Number_Game/program.py
+++ 02_Guess_Number_Game/program.py
@@ -11,3 +11,9 @@ guess_text = input('Guess a number between 0 and 100: ')
 
 guess = int(guess_text)
 
+if guess < the_number:
+    print('too low')
+elif guess > the_number:
+    print('too high')
+else:
+    print('you win!')
