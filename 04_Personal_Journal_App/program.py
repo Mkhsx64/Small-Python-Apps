f672c32cbb1dc0e5ed09785ae2140e2be59eabef
Mkhs <jonbecher@microagenetworks.com>
2020-02-28 22:16:41 -0500

    Added list for use with adding and listing journal entries.

--- 04_Personal_Journal_App/program.py
+++ 04_Personal_Journal_App/program.py
@@ -13,24 +13,28 @@ def print_header():
 
 def run_event_loop():
     print('What do you want to do with your journal?')
-    
     cmd = None
+    journal_data = []
+    
     while cmd != 'x':
         cmd = input('[L]ist entries, [A]dd an entry, E[x]it: ')
         cmd = cmd.lower().strip()
         if cmd == 'l':
-            list_entries()
+            list_entries(journal_data)
         elif cmd == 'a':
-            add_entry()
+            add_entry(journal_data)
         elif cmd != 'x':
             print("Sorry, we don't understand '{}'".format(cmd))
+    
+    print('Done, goodbye.')
 
 
-def list_entries():
-    print('Listing...')
+def list_entries(data):
+    print(data)
 
-def add_entry():
-    print('Adding...')
+def add_entry(data):
+    text = input('type your entry, <enter> to exit. ')
+    data.append(text)
 
 
 main()
