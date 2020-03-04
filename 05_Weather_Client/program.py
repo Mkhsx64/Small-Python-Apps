a1ec929d0bf39ce76b298bedb467f454c35acf7f
Mkhs <jonbecher@microagenetworks.com>
2020-03-04 04:08:47 -0500

    Defined main method and calling it. As well as logic that we need to accomplish

---
+++ 05_Weather_Client/program.py
@@ -0,0 +1,14 @@
+
+def main():
+    #print the header
+    #get zipcode from user
+    #get html from the web
+    #parse the html
+    #display the forecast
+    pass
+
+
+if __name__ == '__main':
+    main()
+
+
