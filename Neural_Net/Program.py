7fc392dab72263981199cb1846f6ca7384ae2f39
Mkhsx64 <jonbecher@microagenetworks.com>
2022-06-22 09:54:08 -0400

    flow of computations inside the neural net

--- Neural_Net/Program.py
+++ Neural_Net/Program.py
@@ -32,3 +32,9 @@ weights_2 = [2.17, 0.32]
 dot_product_1 = np.dot(input_vector, weights_1)
 
 print(f"The dot product is: {dot_product_1}")
+
+#going to use sigmoid activation function with 2 layers
+#0 to 1; >.5 -> 1; >.5 -> 0
+#input----\
+#          ---> (Layer 1) -> dot_product -> dot_product_result ------> sum -> layer_1_result -> layer 2 -> sigmoid -> prediction 
+# weights-/                                                    bias-/
