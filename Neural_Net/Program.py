580dfb84502836f1d586b5971d7168c39c4f2acd
Mkhsx64 <jonbecher@microagenetworks.com>
2022-06-22 10:15:17 -0400

    Prediction Logic using dot product for layer 1 and sigmoid for layer 2

--- Neural_Net/Program.py
+++ Neural_Net/Program.py
@@ -8,3 +8,18 @@ import numpy as np
 #          ---> (Layer 1) -> dot_product -> dot_product_result ------> sum -> layer_1_result -> layer 2 -> sigmoid -> prediction 
 #weights--/                                                    bias-/
 
+input_vector = np.array([1.66, 1.56])
+weights_1 = np.array([1.45, -0.66])
+bias = np.array([0.0])
+
+def sigmoid(x):
+    return 1/ (1 + np.exp(-x))
+
+def make_prediction(input_vector, weights, bias):
+    layer_1 = np.dot(input_vector, weights) + bias
+    layer_2 = sigmoid(layer_1)
+    return layer_2
+
+prediction = make_prediction(input_vector, weights_1, bias)
+
+print(f"The prediction result is: {prediction}")
