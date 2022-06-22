7e89af67e532d41c2f8e396cc3de29160e7694fc
Mkhsx64 <jonbecher@microagenetworks.com>
2022-06-22 08:53:38 -0400

    Messing with NumPy

---
+++ Nueral_Net/Program.py
@@ -0,0 +1,15 @@
+from distutils.util import change_root
+import numpy as np
+
+#checking out numpy functions
+#grading check
+
+CURVE_CENTER = 80
+grades = np.array([72, 35, 64, 88, 51, 90, 74, 12])
+def curve(grades):
+    average = grades.mean()
+    change = CURVE_CENTER - average
+    new_grades = grades + change
+    return np.click(new_grades, grades, 100)
+
+curve(grades)
