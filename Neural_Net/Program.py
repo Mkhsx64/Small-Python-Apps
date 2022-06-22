d69f26fdcf35d1738435f00985ef24156b8cf06b
Mkhsx64 <jonbecher@microagenetworks.com>
2022-06-22 09:54:45 -0400

    spacing

--- Neural_Net/Program.py
+++ Neural_Net/Program.py
@@ -37,4 +37,4 @@ print(f"The dot product is: {dot_product_1}")
 #0 to 1; >.5 -> 1; >.5 -> 0
 #input----\
 #          ---> (Layer 1) -> dot_product -> dot_product_result ------> sum -> layer_1_result -> layer 2 -> sigmoid -> prediction 
-# weights-/                                                    bias-/
+#weights--/                                                    bias-/
