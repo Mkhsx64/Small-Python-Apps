ae0d899972c1c42a95c3e67c74d85a64a80606ae
Mkhsx64 <jonbecher@microagenetworks.com>
2022-06-27 11:02:52 -0400

    ...

--- Neural_Net/Program.py
+++ Neural_Net/Program.py
@@ -9,27 +9,10 @@ from NeuralNetwork import NeuralNetwork
 #          ---> (Layer 1) -> dot_product -> dot_product_result ------> sum -> layer_1_result -> layer 2 -> sigmoid -> prediction 
 #weights--/                                                    bias-/
 
+input_vector = np.array([2, 1.5])
 
-#Using a cost function to measure the error; MSE (mean squared error)
-target = 0
-#Compute difference between the prediction and the target; multiply the result by itself
-mse = np.square(prediction - target)
+learning_rate = 0.1
 
-print(f"Prediction: {prediction}; Error: {mse}")
+neural_network = NeuralNetwork(learning_rate)
 
-#To know which direction to reduce the error, going to use derivative
-derivative = 2 * (prediction - target)
-
-print(f"The derivative is {derivative}")
-
-#updating the weights
-weights_1 = weights_1 - derivative
-
-prediction = make_prediction(input_vector, weights_1, bias)
-
-error = (prediction - target) ** 2
-
-print(f"Prediction: {prediction}; Error: {error}")
-
-
-
+print(neural_network.predict(input_vector))
