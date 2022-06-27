a4c3284e8133301b7535d7799bf7cd00508ffadb
Mkhsx64 <jonbecher@microagenetworks.com>
2022-06-27 10:36:59 -0400

    Created class and init method

---
+++ Neural_Net/NeuralNetwork.py
@@ -0,0 +1,7 @@
+class NeuralNetwork:
+    def __init__(self, learning_rate):
+        self.weights = np.array([np.random.randn(), np.random.randn()])
+        self.bias = np.random.randn()
+        self.learning_rate = learning_rate
+    
+    
