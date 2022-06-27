af8b1567366e88d56396a9828dd4fda8bb23a0bc
Mkhsx64 <jonbecher@microagenetworks.com>
2022-06-27 10:53:26 -0400

    Added _compute_gradients method (chain rule with backpropagation

--- Neural_Net/NeuralNetwork.py
+++ Neural_Net/NeuralNetwork.py
@@ -18,4 +18,21 @@ class NeuralNetwork:
         prediction = layer_2
         return prediction
 
-    
+    def _compute_gradients(self, input_vector, target):
+        layer_1 = np.dot(input_vector, self.weights) + self.bias
+        layer_2 = self._sigmoid(layer_1)
+        prediction = layer_2
+
+        derror_dprediction = 2 * (prediction - target)
+        dprediction_dlayer1 = self._sigmoid_deriv(layer_1)
+        dlayer1_dbias = 1
+        dlayer1_dweights = (0 * self.weights) + (1 * input_vector)
+
+        derror_dbias = (
+            derror_dprediction * dprediction_dlayer1 * dlayer1_dbias
+        )
+        derror_dweights = (
+            derror_dprediction * dprediction_dlayer1 * dlayer1_dweights
+        )
+
+        return derror_dbias, derror_dweights
--- Neural_Net/Program.py
+++ Neural_Net/Program.py
@@ -9,17 +9,6 @@ from NeuralNetwork import NeuralNetwork
 #          ---> (Layer 1) -> dot_product -> dot_product_result ------> sum -> layer_1_result -> layer 2 -> sigmoid -> prediction 
 #weights--/                                                    bias-/
 
-input_vector = np.array([1.66, 1.56])
-weights_1 = np.array([1.45, -0.66])
-bias = np.array([0.0])
-
-
-
-
-
-prediction = make_prediction(input_vector, weights_1, bias)
-
-print(f"The prediction result is: {prediction}")
 
 #Using a cost function to measure the error; MSE (mean squared error)
 target = 0
@@ -42,16 +31,5 @@ error = (prediction - target) ** 2
 
 print(f"Prediction: {prediction}; Error: {error}")
 
-#Applying the chain rule and using backpropagation
 
-
-
-derror_dprediction = 2 * (prediction - target)
-layer_1 = np.dot(input_vector, weights_1) + bias
-dprediction_dlayer1 = sigmoid_deriv(layer_1)
-dlayer1_dbias = 1
-
-derror_dbias = (
-    derror_dprediction * dprediction_dlayer1 * dlayer1_dbias
-)
 
