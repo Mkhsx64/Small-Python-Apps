503f9b4ba18d6f8430c49fefa88e31a834915cac
Mkhs <jonbecher@microagenetworks.com>
2022-06-30 21:31:51 -0400

    Update NeuralNetwork.py

--- Neural_Net/NeuralNetwork.py
+++ Neural_Net/NeuralNetwork.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 #going to use sigmoid activation function with 2 layers
-#0 to 1; >.5 -> 1; >.5 -> 0
+#0 to 1; >.5 -> 1; <.5 -> 0
 #input----\
 #          ---> (Layer 1) -> dot_product -> dot_product_result ------> sum -> layer_1_result -> layer 2 -> sigmoid -> prediction 
 #weights--/                                                    bias-/
