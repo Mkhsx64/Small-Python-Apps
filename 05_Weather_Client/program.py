1c0e9d7d2ca5c41d247dfa153fd74673f58adf89
Mkhs <jonbecher@microagenetworks.com>
2022-08-15 13:48:27 -0400

    updated comments and created a function to convert plaintext provided by the user to passable data

--- 05_Weather_Client/program.py
+++ 05_Weather_Client/program.py
@@ -4,11 +4,17 @@ def main():
     # get the location from the user
     locationText = input('Where do you want the weather report (e.g. Portland, US)? ')
     print(f"You selected {locationText}")
+
+    # convert plaintext over to data we can use
+    location = convertPlaintextLocation(locationText)
+    print(f"Location = {location}")
     
     get_html()
     parse_html()
     display()
 
+def convertPlaintextLocation(plaintText):
+    pass
 
 def display():
     pass
@@ -23,9 +29,11 @@ def get_html():
 
 
 def print_header():
+    # heading for the application
     print('--------------------------------')
     print('         WEATHER CLIENT         ')
     print('--------------------------------')
+    print()
 
 
 if __name__ == '__main__':
