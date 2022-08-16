4b44b71096f0b70b537186a385dade724b9a056a
Mkhs <jonbecher@microagenetworks.com>
2022-08-16 03:07:38 -0400

    updated interpreter to use venv to install requests locally for this program, set up logic for the GET request, tested and working, set up error handling

--- 05_Weather_Client/program.py
+++ 05_Weather_Client/program.py
@@ -1,5 +1,5 @@
 import collections
-
+import requests
 
 Location = collections.namedtuple('Location', 'city state country')
 
@@ -12,7 +12,24 @@ def main():
 
     # convert plaintext over to data we can use
     loc = convertPlaintextLocation(locationText)
-    print(loc)
+
+    # get report from the API
+    key = '6215b88044902e8f262cd476407e0a26'
+    data = callWeatherAPI(loc, key)
+    print(data)
+    
+
+def callWeatherAPI(location, APIkey):
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={location.city},{location.country}&units=imperial&appid={APIkey}'
+    if location.state == '':
+        url = url = f'https://api.openweathermap.org/data/2.5/weather?q={location.city},{location.state},{location.country}&units=imperial&appid={APIkey}'
+    resp = requests.get(url)
+    if resp.status_code in {400, 404, 500}:
+        print(f"Error: {resp.text}.")
+        return None
+    data = resp.json()
+
+    return data
 
 def convertPlaintextLocation(plaintText):
     # if no input return None
